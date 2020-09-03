018d5771e0ec0bac5df0855efb63733f7dbe1e71
liuxiaofei <liuxiaofei@geyecloud.com>
2020-09-03 11:43:37 +0800

    eta data test

---
+++ test.py
@@ -0,0 +1,111 @@
+from sklearn.datasets import load_iris
+from boosting import BoostingModules
+from ann import NeuralNetwork
+import pandas as pd
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import cross_val_score
+from utils.utils import train_data_split
+import xgboost as _xgb
+from sklearn.metrics import f1_score
+from utils.tools import eda
+
+
+def load_data():
+    # 读取莺尾花数据集
+    iris = load_iris()
+    data = pd.DataFrame(iris.data, columns=iris.feature_names)
+    target = pd.DataFrame(iris.target, columns=['target'])
+    data_set = pd.concat([data, target], axis=1)
+    return data_set
+
+
+def load_eta_data():
+    # (64554, 34)
+    file_path = 'E:/竞赛数据/ETA/MTA-KDD-19-master/'
+
+    d = pd.read_csv(file_path + 'kdd_19_data.csv')
+    # eda(d, '')
+    X_t, X_v, y_t, y_v = train_data_split(d, _size=0.2)
+    X_t['label'] = y_t
+    X_v['label'] = y_v
+    X_t['id'] = X_t.index
+    X_t['id'] = X_t.index
+    # X_t.to_csv(file_path + 'eta_train.csv')
+    # X_v.to_csv(file_path + 'eta_test.csv')
+    return X_t, X_t
+
+
+def train_handel(data):
+    label = data['label']
+    feature = data.drop(['id', 'label'], axis=1)
+    return feature, label
+
+
+def analysis(train):
+    # boost = BoostingModules(train)
+    # # boost.rounds = 10
+    # xgb_params = {
+    #               'booster': 'gbtree',
+    #               'objective': 'binary:logistic',
+    #               'eval_metric': 'auc',
+    #               'verbosity': 0
+    #               }
+    # xgb_model = boost.xgb_model(xgb_params)
+    #
+    # lgb_params = {
+    #               'boosting_type': 'gbdt',
+    #               'objective': 'binary',
+    #               'metric': {'binary_logloss', 'auc'},
+    #              }
+    # lgb_model = boost.lgb_model(lgb_params)
+    #
+    # cb_model = boost.cb_model()
+
+    # ANN
+    X_t, X_v, y_t, y_v = train_data_split(train)
+    ann_m = NeuralNetwork([33, 68, 68, 1])
+    ann_m.fit(X_t, y_t, epochs=10, learning_rate=0.1)
+
+    # return xgb_model, lgb_model, cb_model, ann_m
+    return ann_m
+
+
+def cross_val(dt, test_df, k=3):
+    kf = StratifiedKFold(n_splits=k, shuffle=True, random_state=2020)
+    _result = 0  # 最终结果
+    data_k, label_k = train_handel(dt)
+    for train_index, test_index in kf.split(data_k, label_k):
+        X_train_p, y_train_p = data_k.iloc[train_index], label_k.iloc[train_index]
+        X_valid_p, y_valid_p = data_k.iloc[test_index], label_k.iloc[test_index]
+        xgb_params = {
+            'booster': 'gbtree',
+            'objective': 'binary:logistic',
+            'eval_metric': 'auc',
+            'verbosity': 0
+        }
+        xgb_m = BoostingModules(train)
+        xgb_m.X_t, xgb_m.X_v, xgb_m.y_t, xgb_m.y_v = X_train_p, X_valid_p, y_train_p, y_valid_p
+        xgb_model = xgb_m.xgb_model(xgb_params)
+        _result += xgb_model.predict(_xgb.DMatrix(test_df)) / k
+    return [round(x) for x in _result]
+
+
+if __name__ == '__main__':
+    train, test = load_eta_data()
+    test_feature, test_label = train_handel(test)
+
+    xgb_c, lgb_c, cb_c, ann_c = analysis(train)
+    # test_xgb_res = xgb_c.predict(_xgb.DMatrix(test_feature))
+    # test_lgb_res = lgb_c.predict(test_feature)
+    # test_cb_res = cb_c.predict(test_feature)
+    test_ann_res = ann_c.predict(test_feature)
+
+    # test_res = cross_val(train, test_feature, k=10)
+
+    # xgb_f1_score = f1_score(test_xgb_res, test_label)  # 3
+    # lgb_f1_score = f1_score(test_lgb_res, test_label)  # 33
+    # cb_f1_score = f1_score(test_cb_res, test_label)  # 245
+    ann_f1_score = f1_score(test_ann_res, test_label)  # 245
+
+    # print(xgb_f1_score, lgb_f1_score, cb_f1_score, ann_f1_score)
+    print(ann_f1_score)
