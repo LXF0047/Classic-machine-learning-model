df97b099d1a3800da35cdc591ce55788faca7f04
liuxiaofei <liuxiaofei@geyecloud.com>
2020-09-30 15:42:58 +0800

    delete a file

--- load_model.py
+++
@@ -1,7 +0,0 @@
-import xgboost as xgb
-import lightgbm as lgb
-from catboost import CatBoostClassifier
-import pandas as pd
-
-d = pd.read_csv('/data0/new_workspace/mlxtend_dga_bin_20190307/merge/new_feature/legit.csv').head(50)
-print(d.astype('int'))
